2fff5fd45e23e9684d24e9bc5cc053e52c08f9ef
Yehor <egorzvoznikov@gmail.com>
2025-02-18 13:33:37 +0200

    Add Die Behavior To Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,7 +1,28 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private PlayerAnimation _playerAnimation;
+    [SerializeField] private int _health;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.TryGetComponent(out Enemy enemy))
+        {
+            if (_health > 0)
+            {
+                _health--;
+                _playerAnimation.SetFade();
+
+                if (_health <= 0)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Coin coin))
